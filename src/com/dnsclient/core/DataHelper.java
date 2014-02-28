ed5f4e4fa8f468a1fbf918bceb92bc65e9e395db
Karuto Z <iamkaruto@gmail.com>
2014-02-27 19:09:59 -0800

    Printed out chars, detect QNAME

--- src/com/dnsclient/core/DataHelper.java
+++ src/com/dnsclient/core/DataHelper.java
@@ -14,6 +14,14 @@ public class DataHelper {
     return instance;
   }
 
+  protected Boolean isZeroByte(byte b) {
+    Boolean isZero = false;
+    if (getBitsFromByte(b).equalsIgnoreCase("00000000")) {
+      isZero = true;
+    }
+    return isZero;
+  }
+  
   /* Helper function to print a single bit at given index from a byte */
   protected void printBitFromByte(byte b, int index) {
     assert(index >= 0);
@@ -45,7 +53,7 @@ public class DataHelper {
     }
   }
     
-  /* Helper function to print bits from a byte array, with segments */
+  /* Helper function to print bits from a byte array with end point */
   protected void printBitsFromByteArray(byte[] bytes, int byteCounts) {
     assert (byteCounts < bytes.length);
     for (int i = 0; i < byteCounts; i++) {
@@ -54,7 +62,19 @@ public class DataHelper {
     }
   }
   
-  /* Helper function to print bits from a byte array, with options */
+  /* Helper function to print bits from a byte array with start + end points 
+   * Note: this will print exactly bytes[start] to bytes[end]
+   */
+  protected void printBitsFromByteArray(byte[] bytes, int start, int end) {
+    assert (start < bytes.length || start >= 0 || start < end);
+    assert (end < bytes.length || end >= 0);
+    for (int i = start; i <= end; i++) {
+      System.out.println("Byte #" + i);   
+      printBitsFromByte(bytes[i]);
+    }
+  }
+  
+  /* Helper function to print bits from a byte array with output options */
   protected void printBitsFromByteArray(byte[] bytes, Boolean hasCounter) {
     for (int i = 0; i < bytes.length; i++) {
       if (hasCounter) {
@@ -83,6 +103,13 @@ public class DataHelper {
     String bits = String.format("%8s", 
         Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
     return bits;
+  }
+  
+  protected int getContentFromByte(byte b) {
+    String bits = String.format("%8s", 
+        Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
+    int charCode = Integer.parseInt(bits, 2);
+    return charCode;
   }
 
 }
--- src/com/dnsclient/core/ResponseGenerator.java
+++ src/com/dnsclient/core/ResponseGenerator.java
@@ -8,14 +8,27 @@ public class ResponseGenerator {
   public ResponseGenerator(byte[] responseData) {
     this.data = responseData;
     this.helper = DataHelper.getInstance();
-    parse();
+    parseResponse();
   }
   
-  protected void parse() {
+  protected void parseResponse() {
     System.out.println("======= RESPONSE ====== ");
-    helper.printBitsFromByteArray(data, 3);
+//    helper.printBitsFromByteArray(data, 12);
+//    helper.printBitsFromByteArray(data, 12, 20);
     helper.printBitFromByte(data[3], 0);
     if (helper.getBitFromByte(data[3],0) == '1') {
+      // Valid DNS response, keep parsing
+      
+      // Loop until reaching first zero byte, which is end of QNAME
+      int i = 12;
+      while (!helper.isZeroByte(data[i])) {
+        System.out.print((char) data[i]);
+//        System.out.println(helper.getContentFromByte(data[i]));
+        i++;
+      }
+      System.out.println();
+      System.out.println(i);
+    helper.printBitsFromByteArray(data, 23, 25); // byte[24] is where it ends (endblock)
       
     } else {
       // Invalid because QR != 1, doesn't match response type
